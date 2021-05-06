4b1686c6b396a90f7cb7f2deaffdc3b321fafb08
ss555 <5sardorisrailov5@gmail.com>
2021-05-07 00:32:53 +0200

    right params

--- parameters.py
+++ parameters.py
@@ -12,7 +12,19 @@ sac_cartpole50={
     log_std_init: -0.9655242596737095,
     net_arch: [256,256]
 }
-
+dqn_20={
+    gamma: 0.95,
+    lr: 0.0018307754257612685,
+    batch_size: 512,
+    buffer_size: 1000000,
+    exploration_final_eps: 0.02633362231811897,
+    exploration_fraction: 0.22175283117941602,
+    target_update_interval: 1,
+    learning_starts: 10000,
+    train_freq: 1000,
+    subsample_steps: 2,
+    net_arch: [64,64]
+}
 noise_rad={
 
 }
